12394532fed864f966cd475c39d5520ca76ce1c8
Kevin Wong <kevinwong973@gmail.com>
2024-06-05 22:23:04 -0700

    pushing part 1 ex.

---
+++ Part1/src/App.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/prop-types */
+const Header = ({ course }) => (
+  <div>
+    <h1>{course}</h1>
+  </div>
+)
+
+const Part = ({ part, exercises }) => {
+  return (
+    <div>
+      <p>{part} {exercises}</p>
+    </div>
+  )
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+};
+
+
+const Total = ({ parts }) => {
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return (
+    <div>
+      <p>Number of exercises {totalExercises}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+
+
+};
+
+
+export default App
